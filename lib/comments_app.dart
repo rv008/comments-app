f40d3fb5fa242286ac8d9c029fce791ca30bd4b7
Ronald <ronaldvarghese10@gmail.com>
2024-07-27 17:07:42 +0530

    riverpod packages

---
+++ lib/comments_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CommentsApp extends StatelessWidget {
+  const CommentsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Comments App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
